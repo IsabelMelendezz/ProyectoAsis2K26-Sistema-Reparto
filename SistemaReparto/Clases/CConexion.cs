3bd114983641feadddd6081f36e067dd5fcf2456
CristianSipac807 <cdavids0807@gmail.com>
2026-07-24 18:40:17 -0600

    Subiendo clase conexion

---
+++ SistemaReparto/Clases/CConexion.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaReparto.Clases
+{
+    internal class CConexion
+    {
+        MySqlConnection conex = new MySqlConnection();
+        static string servidor = "localhost";
+        static string bd = "sistema_reparto";
+        static string usuario = "root";
+        static string password = "Root@123456";
+        static string puerto = "3306";
+
+        string cadenaConexion = "server=" + servidor + ";" + "port=" + puerto + ";" + "user id=" + usuario + ";" + "password=" + password + ";" + "database=" + bd + ";";
+      //string cadenaConexion = "server=" + servidor + ";" + "port=" + puerto + ";" + "user id=" + usuario + ";" + "password=" + password + ";" + "database" + bd + ";";
+
+        public MySqlConnection establecerConexion()
+        {
+            try
+            {
+                conex.ConnectionString = cadenaConexion;
+                conex.Open();
+                //MessageBox.Show("Se conecto a la base de datos");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se conecto a la base de datos, error: " + ex.ToString());
+            }
+            return conex;
+        }
+
+        public void cerrarConexion() { conex.Close(); }
+    }
+}
